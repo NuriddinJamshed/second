f8d5c4c53eef8e08669e6203ec50a77ceb28d8b5
NuriddinJamshed <j.nuriddin2021@gmail.com>
2025-04-11 15:21:16 +0500

    dskjhfk

---
+++ my-app/src/App.jsx
@@ -0,0 +1,300 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+        <div className='header'>
+            <div className='nav'>
+                <img src="/img/logo (15).png" alt="" />
+                <div className='menu'>
+                    <a href="#">About</a>
+                    <a href="#">Features</a>
+                    <a href="#">Pricing</a>
+                    <a href="#">Testimonials</a>
+                    <a href="#">Contact</a>
+                </div>
+                <div className='sign'>
+                    <a href="#">Log in</a>
+                    <button>Sign up</button>
+                </div>
+            </div>
+            <div className='sectionHeader'>
+                <div className='leftHeader'>
+                    <h1>Get access to worlwide content</h1>
+                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered.</p>
+                    <p>Start from <samp>$2.99</samp> / month</p>
+                    <button>Get Started</button>
+                    <span>* 30-Days money back gurantee</span>
+                </div>
+                <img src="/img/Group (6).png" alt="" />
+            </div>
+        </div>
+            <img src="/img/Group 6.png" alt="foto" className='imgCenter' />
+        <div className='main'>
+            <div className='section-1'>
+                <img src="/img/Group (7).png" alt="foto" />
+                <div className='right'>
+                    <h1 className='title'>One gateway endless entertainment</h1>
+                    <p className='description'>Content at the tip of your fingers! With thousands of servers and millions of IPs, you can access websites and enjoy your favourite movies, TV shows, games, and more; in over 190 countries.</p>
+                    <div className='images'>
+                        <img src="/img/Group (15).png" alt="" />
+                        <img src="/img/Group (16).png" alt="" />
+                        <img src="/img/Group (17).png" alt="" />
+                        <img src="/img/Group (18).png" alt="" />
+                    </div>
+                </div>
+            </div>
+        <div className='section-1'>
+            <div className='left'>
+                <div className='title'>Putting power back into your hands</div>
+                <p className='description'>Instant protection to enhance your online privacy. Browse the internet safety and anonymously.</p>
+                <button className='btnMain'>Get Started</button>
+            </div>
+            <img src="/img/Group (8).png" alt="" />
+        </div>
+        <div className='section-1'>
+            <img src="/img/Group (9).png" alt="" />
+            <div className='right'>
+                <h1 className='title'>30 Days money back guarantee</h1>
+                <p className='description'>Since we trust that you will fall in love with vping, we offer our users the option to try it for 30 days. If you don’t enjoy the experience, we will give you a 100% refund.</p>
+                <button className='btnMain'>Get Started</button>
+            </div>
+        </div>
+        <div className='section-2'>
+            <h1 className='title'>Save big with our annual plans</h1>
+            <p className='description'>Sign up and save a ton! No logs. No hidden fees.</p>
+            <div className='box'>
+                <div className='card'>
+                    <button className='btn-1 btn'>Premium Plan</button>
+                    <div className='div-text'>
+                        <h4>3 Years</h4>
+                        <h4><samp>$2.99</samp> / month</h4>
+                        <div className='prise-line'>
+                            <h4>$107.55</h4>
+                            <p>$537.30</p>
+                        </div>
+                        <p>Build every 3 years</p>
+                    </div>
+                    <div className='lists'>
+                        <div className='listDiv'>
+                            <img src="img/Vector (4).png" alt="" />
+                            <p className='list'>Unlimited bandwitch</p>
+                        </div>
+                        <div className='listDiv'>
+                            <img src="img/Vector (4).png" alt="" />
+                            <p className='list'>Encrypted connection</p>
+                        </div>
+                        <div className='listDiv'>
+                            <img src="img/Vector (4).png" alt="" />
+                            <p className='list'>No traffic logs</p>
+                        </div>
+                        <div className='listDiv'>
+                            <img src="img/Vector (4).png" alt="" />
+                            <p className='list'>Works on all devices</p>
+                        </div>
+                        <div className='listDiv'>
+                            <img src="img/Vector (4).png" alt="" />
+                            <p className='list'>Instant setup</p>
+                        </div>
+                        <div className='listDiv'>
+                            <img src="img/Vector (4).png" alt="" />
+                            <p className='list'>Torrent allowed</p>
+                        </div>
+                    </div>
+                    <button className='subBtn'>Subscribe</button>
+                </div>
+                <div className='card card-2'>
+                    <button className='btn-2 btn'>Premium Plan</button>
+                    <div className='div-text'>
+                        <h4>3 Years</h4>
+                        <h4><samp>$2.99</samp> / month</h4>
+                        <div className='prise-line'>
+                            <h4>$107.55</h4>
+                            <p>$537.30</p>
+                        </div>
+                        <p>Build every 3 years</p>
+                    </div>
+                    <div className='lists'>
+                        <div className='listDiv'>
+                            <img src="img/Vector (4).png" alt="" />
+                            <p className='list'>Unlimited bandwitch</p>
+                        </div>
+                        <div className='listDiv'>
+                            <img src="img/Vector (4).png" alt="" />
+                            <p className='list'>Encrypted connection</p>
+                        </div>
+                        <div className='listDiv'>
+                            <img src="img/Vector (4).png" alt="" />
+                            <p className='list'>No traffic logs</p>
+                        </div>
+                        <div className='listDiv'>
+                            <img src="img/Vector (4).png" alt="" />
+                            <p className='list'>Works on all devices</p>
+                        </div>
+                        <div className='listDiv'>
+                            <img src="img/Vector (4).png" alt="" />
+                            <p className='list'>Instant setup</p>
+                        </div>
+                        <div className='listDiv'>
+                            <img src="img/Vector (4).png" alt="" />
+                            <p className='list'>Torrent allowed</p>
+                        </div>
+                    </div>
+                    <button className='subBtn subBtn-2'>Subscribe</button>
+                </div>
+                <div className='card'>
+                    <button className='btn-3 btn'>Premium Plan</button>
+                    <div className='div-text'>
+                        <h4>3 Years</h4>
+                        <h4><samp>$2.99</samp> / month</h4>
+                        <div className='prise-line'>
+                            <h4>$107.55</h4>
+                            <p>$537.30</p>
+                        </div>
+                        <p>Build every 3 years</p>
+                    </div>
+                    <div className='lists'>
+                        <div className='listDiv'>
+                            <img src="img/Vector (4).png" alt="" />
+                            <p className='list'>Unlimited bandwitch</p>
+                        </div>
+                        <div className='listDiv'>
+                            <img src="img/Vector (4).png" alt="" />
+                            <p className='list'>Encrypted connection</p>
+                        </div>
+                        <div className='listDiv'>
+                            <img src="img/Vector (4).png" alt="" />
+                            <p className='list'>No traffic logs</p>
+                        </div>
+                        <div className='listDiv'>
+                            <img src="img/Vector (4).png" alt="" />
+                            <p className='list'>Works on all devices</p>
+                        </div>
+                        <div className='listDiv'>
+                            <img src="img/Vector (4).png" alt="" />
+                            <p className='list'>Instant setup</p>
+                        </div>
+                        <div className='listDiv'>
+                            <img src="img/Vector (4).png" alt="" />
+                            <p className='list'>Torrent allowed</p>
+                        </div>
+                    </div>
+                    <button className='subBtn'>Subscribe</button>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div className='section-3'>
+            <img className='img-z' src="/img/Ellipse 14 (Stroke).png" alt="" />
+            <div className='dialog'>
+                <h1 className='ti'>A premium experience</h1>
+                <div className='icons'>
+                    <div className='iconDiv'>
+                        <img src="/img/Group 1.png" alt="" />
+                        <h2>No Limits</h2>
+                        <p className='description'>Enjoy unlimited data - nothing will stop you!</p>
+                    </div>
+                    <div className='iconDiv'>
+                        <img src="/img/Group 1.png" alt="" />
+                        <h2>No Limits</h2>
+                        <p className='description'>Enjoy unlimited data - nothing will stop you!</p>
+                    </div>
+                    <div className='iconDiv'>
+                        <img src="/img/Group 1.png" alt="" />
+                        <h2>No Limits</h2>
+                        <p className='description'>Enjoy unlimited data - nothing will stop you!</p>
+                    </div>
+                    <div className='iconDiv'>
+                        <img src="/img/Group 1.png" alt="" />
+                        <h2>No Limits</h2>
+                        <p className='description'>Enjoy unlimited data - nothing will stop you!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className='section-4'>
+            <h1 className='title'>Questions users are asking</h1>
+            <div className='selectors'>
+                <div className='select'>
+                    <h2>Which plan is right for me?</h2>
+                    <span><svg width="28" height="16" viewBox="0 0 28 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M1.10775 1.21712C1.62641 0.698459 2.46734 0.698459 2.986 1.21712L14 12.2311L25.014 1.21712C25.5327 0.698459 26.3736 0.698459 26.8923 1.21712C27.4109 1.73579 27.4109 2.57671 26.8923 3.09538L14.9391 15.0485C14.4205 15.5672 13.5795 15.5672 13.0609 15.0485L1.10775 3.09538C0.589084 2.57671 0.589084 1.73579 1.10775 1.21712Z" fill="#666E6D"/></svg></span>
+                </div>
+                <div className='select'>
+                    <h2>Which plan is right for me?</h2>
+                    <span><svg width="28" height="16" viewBox="0 0 28 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M1.10775 1.21712C1.62641 0.698459 2.46734 0.698459 2.986 1.21712L14 12.2311L25.014 1.21712C25.5327 0.698459 26.3736 0.698459 26.8923 1.21712C27.4109 1.73579 27.4109 2.57671 26.8923 3.09538L14.9391 15.0485C14.4205 15.5672 13.5795 15.5672 13.0609 15.0485L1.10775 3.09538C0.589084 2.57671 0.589084 1.73579 1.10775 1.21712Z" fill="#666E6D"/></svg></span>
+                </div>
+                <div className='select'>
+                    <h2>Which plan is right for me?</h2>
+                    <span><svg width="28" height="16" viewBox="0 0 28 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M1.10775 1.21712C1.62641 0.698459 2.46734 0.698459 2.986 1.21712L14 12.2311L25.014 1.21712C25.5327 0.698459 26.3736 0.698459 26.8923 1.21712C27.4109 1.73579 27.4109 2.57671 26.8923 3.09538L14.9391 15.0485C14.4205 15.5672 13.5795 15.5672 13.0609 15.0485L1.10775 3.09538C0.589084 2.57671 0.589084 1.73579 1.10775 1.21712Z" fill="#666E6D"/></svg></span>
+                </div>
+                <div className='select'>
+                    <h2>Which plan is right for me?</h2>
+                    <span><svg width="28" height="16" viewBox="0 0 28 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M1.10775 1.21712C1.62641 0.698459 2.46734 0.698459 2.986 1.21712L14 12.2311L25.014 1.21712C25.5327 0.698459 26.3736 0.698459 26.8923 1.21712C27.4109 1.73579 27.4109 2.57671 26.8923 3.09538L14.9391 15.0485C14.4205 15.5672 13.5795 15.5672 13.0609 15.0485L1.10775 3.09538C0.589084 2.57671 0.589084 1.73579 1.10775 1.21712Z" fill="#666E6D"/></svg></span>
+                </div>
+                <div className='select'>
+                    <h2>Which plan is right for me?</h2>
+                    <span><svg width="28" height="16" viewBox="0 0 28 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M1.10775 1.21712C1.62641 0.698459 2.46734 0.698459 2.986 1.21712L14 12.2311L25.014 1.21712C25.5327 0.698459 26.3736 0.698459 26.8923 1.21712C27.4109 1.73579 27.4109 2.57671 26.8923 3.09538L14.9391 15.0485C14.4205 15.5672 13.5795 15.5672 13.0609 15.0485L1.10775 3.09538C0.589084 2.57671 0.589084 1.73579 1.10775 1.21712Z" fill="#666E6D"/></svg></span>
+                </div>
+                <div className='select'>
+                    <h2>Which plan is right for me?</h2>
+                    <span><svg width="28" height="16" viewBox="0 0 28 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M1.10775 1.21712C1.62641 0.698459 2.46734 0.698459 2.986 1.21712L14 12.2311L25.014 1.21712C25.5327 0.698459 26.3736 0.698459 26.8923 1.21712C27.4109 1.73579 27.4109 2.57671 26.8923 3.09538L14.9391 15.0485C14.4205 15.5672 13.5795 15.5672 13.0609 15.0485L1.10775 3.09538C0.589084 2.57671 0.589084 1.73579 1.10775 1.21712Z" fill="#666E6D"/></svg></span>
+                </div>
+                <div className='select'>
+                    <h2>Which plan is right for me?</h2>
+                    <span><svg width="28" height="16" viewBox="0 0 28 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M1.10775 1.21712C1.62641 0.698459 2.46734 0.698459 2.986 1.21712L14 12.2311L25.014 1.21712C25.5327 0.698459 26.3736 0.698459 26.8923 1.21712C27.4109 1.73579 27.4109 2.57671 26.8923 3.09538L14.9391 15.0485C14.4205 15.5672 13.5795 15.5672 13.0609 15.0485L1.10775 3.09538C0.589084 2.57671 0.589084 1.73579 1.10775 1.21712Z" fill="#666E6D"/></svg></span>
+                </div>
+                <div className='select'>
+                    <h2>Which plan is right for me?</h2>
+                    <span><svg width="28" height="16" viewBox="0 0 28 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M1.10775 1.21712C1.62641 0.698459 2.46734 0.698459 2.986 1.21712L14 12.2311L25.014 1.21712C25.5327 0.698459 26.3736 0.698459 26.8923 1.21712C27.4109 1.73579 27.4109 2.57671 26.8923 3.09538L14.9391 15.0485C14.4205 15.5672 13.5795 15.5672 13.0609 15.0485L1.10775 3.09538C0.589084 2.57671 0.589084 1.73579 1.10775 1.21712Z" fill="#666E6D"/></svg></span>
+                </div>
+            </div>
+        </div>
+        <div className='footer'>
+            <div className='section1Footer'>
+                <img src="/img/Frame (3).png" alt="" />
+                <div className='rightAs'>
+                    <h1 className='titleFooter'>Get what you need online, completely undetected.</h1>
+                    <p>30 Day money back guarantee. If you’re not 100% satisfied with vping. We’ll refund your payment. No hassle, no risk.</p>
+                    <img className='stars' src="/img/Group 23.png" alt="" />
+                    <p>Start from <samp>$2.99</samp> / month</p>
+                    <button className='getBtn'>Get Started</button>
+                </div>
+            </div>
+            <div className='bottomFooter'>
+                <div className='rightAsaid'>
+                    <img className='logo' src="/img/logo (15).png" alt="" />
+                    <p className='descript'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                </div>
+                <div>
+                    <p className='descript'>Product</p>
+                    <p className='descript'>Download</p>
+                    <p className='descript'>Pricing</p>
+                    <p className='descript'>Server</p>
+                    <p className='descript'>VPN for Business</p>
+                    <p className='descript'>VPN for Non-Profit</p>
+                </div>
+                <div>
+                    <p className='descript'>Company</p>
+                    <p className='descript'>What’s is VPN?</p>
+                    <p className='descript'>FAQ</p>
+                    <p className='descript'>Tuitorials</p>
+                    <p className='descript'>About us</p>
+                    <p className='descript'>Privacy Policy</p>
+                </div>
+                <div>
+                    <p className='descript'>Connect with us</p>
+                    <div className='iconLines'>
+                        <img src="/img/Facebook.png" alt="" />
+                        <img src="/img/Twitter.png" alt="" />
+                        <img src="/img/Instagram.png" alt="" />
+                        <img src="/img/Linkedin.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default App
